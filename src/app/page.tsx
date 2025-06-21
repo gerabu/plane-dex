2fc5313af8470e8f6a6a24443f0e78ff8630a14d
gerabu <aburtogerald@gmail.com>
2025-06-20 21:12:34 -0600

    :lipstick: Add daisyui and configure themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-base-200 text-primary">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 bg-base-100">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem] text-base-content">
             Hello {session?.user.name}
           </h1>
         </div>
